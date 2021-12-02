75876a5d5d4fe6d18b70c70b700f4282e305a18e
TJ <michael.btj@hotmail.com>
2021-12-02 02:55:54 +0000

    fixed bug but now code is ugly

--- fetcher.js
+++ fetcher.js
@@ -10,12 +10,12 @@ const printMessage = function(filepath) {
 }
 
 
-request(url, (body) => {
+request(url, (error, response, body) => {
   // console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
 
-  fs.writeFile(filepath, body, err => { // write html file to this file
+  fs.writeFile(filepath, body, (err) => { // write html file to this file
     if (err) {
       console.error(err);
       return
@@ -28,4 +28,4 @@ request(url, (body) => {
 
 }
 
-fetch(args[0], args[1]);
+fetch(args[0], args[1]); //url filepath
